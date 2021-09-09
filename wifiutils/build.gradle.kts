0b03b02a562da9a88c1ff85c1cb7f66da0f11d74
will <545512533@qq.com>
2021-09-09 15:34:23 +0800

    修改上传脚本

--- wifiutils/build.gradle.kts
+++ wifiutils/build.gradle.kts
@@ -49,3 +49,39 @@ dependencies {
     //addTestDependencies()
 }
 
+val sourcesJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles sources JAR"
+    archiveClassifier.set("sources")
+    from(android.sourceSets.getByName("main").java.srcDirs)
+}
+
+artifacts {
+    archives(sourcesJar)
+}
+
+// https://developer.android.com/studio/build/maven-publish-plugin
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                // https://docs.gradle.org/current/userguide/publishing_maven.html#publishing_maven:repositories
+                url = uri("https://packages.aliyun.com/maven/repository/2132462-release-o7KSVF/")
+                credentials {
+                    username = ""
+                    password = ""
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("release") {
+                // Applies the component for the release build variant.
+                from(components["release"])
+                artifactId = "wifiutils"
+                groupId = "com.xmotion.lib"
+                version = "1.0"
+                artifact(sourcesJar)
+            }
+        }
+    }
+}
